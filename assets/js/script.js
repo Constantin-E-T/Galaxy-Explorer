5f2dcc6248b5fe8637f92a45e6567c2b08c73d61
Emilian Con <emiliancon@Emilians-MacBook-Pro-2.local>
2023-02-07 19:47:31 +0000

    function to retrieve the data from the API

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,21 @@
-alert('hello');
+// alert('hello');
+// Create a function to retrieve the data from the API
+async function getAPOD() {
+    const response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY`);
+    const data = await response.json();
+    console.log(data);
+  
+    // Extract the image URL and description from the data
+    const imageURL = data.url;
+    const description = data.explanation;
+  
+    // Update the header on the main page with the image and description
+    const header = document.getElementById("header");
+    header.style.backgroundImage = `url('${imageURL}')`;
+    header.innerHTML = description;
+  }
+  
+  // Call the function to retrieve the data when the page loads
+  window.onload = function() {
+    getAPOD();
+  };
