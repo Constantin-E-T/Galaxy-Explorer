296a2a0c2d27b2f0603736581a96735a8c2e0c54
Drzazga88 <drzewiecka.katarzyna88@gmail.com>
2023-02-08 20:42:12 +0000

    all available data fetched from API and console logged

--- assets/js/mars_weather.js
+++ assets/js/mars_weather.js
@@ -1,12 +1,34 @@
-const apiUrl = 'https://mars.nasa.gov/rss/api/?feed=weather&category=insight_temperature&feedtype=json&ver=1.0';
+const apiUrl =
+  "https://mars.nasa.gov/rss/api/?feed=weather&category=insight_temperature&feedtype=json&ver=1.0";
 
 fetch(apiUrl)
-  .then(response => response.json())
-  .then(data => {
+  .then((response) => response.json())
+  .then((data) => {
     console.log(data);
     const temperature = data.sol_keys[0];
-    
+    console.log(`Temperature is: ${temperature}`);
+    const tempMin = data[temperature].AT.mn;
+    console.log(`Min_temp: ${tempMin}`);
+    const tempMax = data[temperature].AT.mx;
+    console.log(`Max_temp: ${tempMax}`);
+    const windSpeed = data[temperature].HWS.av;
+    console.log(`Wind speed: ${windSpeed}`);
+    const northernSeason = data[temperature].Season;
+    console.log(`Northern Season: ${northernSeason}`);
+    const southernSeason = data[temperature].Southern_season;
+    console.log(`Southern Season: ${southernSeason}`);
+    const windDirection = data[temperature].WD.most_common.compass_point;
+    console.log(`Wind Direction: ${windDirection}`);
+    const precipitation = data[temperature].PRE.av;
+    console.log(`Precipitation: ${precipitation}`);
+    const monthOrdinal = data[temperature].Month_ordinal;
+    console.log(`Month Ordinal: ${monthOrdinal}`);
+    const date = data[temperature].First_UTC;
+    console.log(`Date: ${date}`);
 
-    document.getElementById('temperature').innerHTML = temperature;
+    document.getElementById(
+      "temperature"
+    ).textContent = `Temperature: ${temperature}°C`;
+    document.getElementById("minTemp").textContent = `Min: ${tempMin}°C`;
   })
-  .catch(error => console.error(error));
+  .catch((error) => console.error(error));
