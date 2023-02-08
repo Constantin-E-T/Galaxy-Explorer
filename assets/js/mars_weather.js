2abf67cae7bca1fe5497361d586573fda70dc538
Emilian Con <emiliancon@Emilians-MacBook-Pro-2.local>
2023-02-08 21:05:34 +0000

    updated mars_weather.js and added btn and ids for data on html

--- assets/js/mars_weather.js
+++ assets/js/mars_weather.js
@@ -1,34 +1,50 @@
-const apiUrl =
-  "https://mars.nasa.gov/rss/api/?feed=weather&category=insight_temperature&feedtype=json&ver=1.0";
+const apiUrl = 'https://mars.nasa.gov/rss/api/?feed=weather&category=insight_temperature&feedtype=json&ver=1.0';
 
+/* Fetch(apiUrl) is calling the apiUrl and returning a promise. */
 fetch(apiUrl)
-  .then((response) => response.json())
-  .then((data) => {
-    console.log(data);
-    const temperature = data.sol_keys[0];
-    console.log(`Temperature is: ${temperature}`);
-    const tempMin = data[temperature].AT.mn;
-    console.log(`Min_temp: ${tempMin}`);
-    const tempMax = data[temperature].AT.mx;
-    console.log(`Max_temp: ${tempMax}`);
-    const windSpeed = data[temperature].HWS.av;
-    console.log(`Wind speed: ${windSpeed}`);
-    const northernSeason = data[temperature].Season;
-    console.log(`Northern Season: ${northernSeason}`);
-    const southernSeason = data[temperature].Southern_season;
-    console.log(`Southern Season: ${southernSeason}`);
-    const windDirection = data[temperature].WD.most_common.compass_point;
-    console.log(`Wind Direction: ${windDirection}`);
-    const precipitation = data[temperature].PRE.av;
-    console.log(`Precipitation: ${precipitation}`);
-    const monthOrdinal = data[temperature].Month_ordinal;
-    console.log(`Month Ordinal: ${monthOrdinal}`);
-    const date = data[temperature].First_UTC;
-    console.log(`Date: ${date}`);
+  .then(response => response.json())
+  .then(data => {
+    const solKeys = data.sol_keys;
+    let solIndex = 0;
 
-    document.getElementById(
-      "temperature"
-    ).textContent = `Temperature: ${temperature}°C`;
-    document.getElementById("minTemp").textContent = `Min: ${tempMin}°C`;
+    /**
+     * It takes the data from the API and displays it on the page
+     */
+    function displayWeatherData() {
+      const sol = solKeys[solIndex];
+      const temperatureData = data[sol].AT;
+      const windData = data[sol].HWS;
+      const date = new Date(data[sol].First_UTC).toDateString();
+
+      document.getElementById('date').innerHTML = date;
+      document.getElementById('temperature').innerHTML = `Min: ${temperatureData.mn} &#8451; Max: ${temperatureData.mx} &#8451;`;
+      document.getElementById('wind').innerHTML = `Speed: ${windData.av} m/s`;
+    }
+
+    /* Calling the function displayWeatherData() */
+    displayWeatherData();
+
+    /* Adding an event listener to the previous button. When the button is clicked, it will decrease
+    the solIndex by 1. If the solIndex is less than 0, it will set the solIndex to the length of the
+    solKeys array minus 1. Then it will call the displayWeatherData() function. */
+    document.getElementById('previous').addEventListener('click', () => {
+      solIndex--;
+      if (solIndex < 0) {
+        solIndex = solKeys.length - 1;
+      }
+      displayWeatherData();
+    });
+
+    /* Adding an event listener to the next button. When the button is clicked, it will increase the
+    solIndex by 1. If the solIndex is greater than or equal to the length of the solKeys array, it
+    will set the solIndex to 0. Then it will call the displayWeatherData() function. */
+    document.getElementById('next').addEventListener('click', () => {
+      solIndex++;
+      if (solIndex >= solKeys.length) {
+        solIndex = 0;
+      }
+      displayWeatherData();
+    });
   })
-  .catch((error) => console.error(error));
+  /* Catching any errors that may occur and logging them to the console. */
+  .catch(error => console.error(error));
